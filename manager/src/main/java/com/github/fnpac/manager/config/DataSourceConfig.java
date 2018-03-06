515fcb54ee509e112d4032e24f2689a14533efa9
liuchunlong <631521383@qq.com>
2018-03-06 18:36:30 +0800

    Update

--- manager/src/main/java/com/github/fnpac/manager/config/DataSourceConfig.java
+++ manager/src/main/java/com/github/fnpac/manager/config/DataSourceConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -19,7 +20,27 @@ import java.sql.SQLException;
  */
 @Configuration
 @PropertySource("classpath:druid.properties")
-@EnableTransactionManagement // enable transaction management
+/**
+ * 启用Spring的注释驱动事务管理功能
+ *
+ * @EnableTransactionManagement 按类型查找容器中任何PlatformTransactionManager bean
+ *
+ * 如果同一个容器中存在两个PlatformTransactionManager bean，
+ * 实现TransactionManagementConfigurer回调接口为@EnableTransactionManagement指定确切事务管理器bean
+ *
+ * 此外，如果存在多个数据源，可以使用分布式事务进行管理
+ * 项目进行读写分离及分库分表，在一个业务中，在一个事务中处理时候将切换多个数据源，需要保证同一事务多个数据源数据的一致性。可以使用atomikos来实现。
+ *
+ * 一般不会出现同时使用多个ORM框架的情况（如Hibernate+JPA+JDO）
+ *
+ * 如果配置多个PlatformTransactionManager bean，会报如下错误：
+ * org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type [org.springframework.transaction.PlatformTransactionManager] is defined: expected single matching bean but found 3: jpaTransactionManager,jdoTransactionManager,jdbcTransactionManager
+ *
+ * 下面有两种处理方式：
+ * 1. 通过@Primary指定候选PlatformTransactionManager bean
+ * 2. 通过@Transactional的transactionManager属性指定事务管理器，参见 com.github.ittalks.fn.annotation.JdbcTransactional
+ */
+@EnableTransactionManagement
 public class DataSourceConfig implements EnvironmentAware {
 
     private Environment environment;
@@ -99,8 +120,14 @@ public class DataSourceConfig implements EnvironmentAware {
         return druidDataSource;
     }
 
+    /**
+     * {@link EnableTransactionManagement} 按类型查找容器中任何PlatformTransactionManager
+     *
+     * @return
+     */
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    @Primary
+    public PlatformTransactionManager jdbcTransactionManager() {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource());
         return transactionManager;
